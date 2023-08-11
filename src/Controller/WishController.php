ce6ee9e153a2fb24d62631895b44396b57253dca
Caliendo Julien <caliendo@hotmail.fr>
2023-08-11 09:49:06 +0200

    Fin du TP5

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
+use App\Repository\WishRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,13 +15,56 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/', name: '_list')]
-    public function list(): Response
+    public function list(
+        WishRepository $wishRepository
+    ): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findBy(
+            ["isPublished" => true],
+            ["date" => "ASC"]
+        );
+        return $this->render(
+            'wish/list.html.twig',
+            compact('wishes')
+        );
     }
-    #[Route('/detail/{id}', name: '_details', requirements: ["id"=>"\d+"])]
-    public function detail(int $id): Response
+
+    #[Route('/detail/{wish}', name: '_details', requirements: ["wish" => "\d+"])]
+    public function detail(Wish $wish): Response
     {
-        return $this->render('wish/detail.html.twig');
+        return $this->render(
+            'wish/detail.html.twig',
+            compact('wish')
+        );
     }
+
+    #[Route('/new', name: '_new')]
+    public function nouveau(
+        EntityManagerInterface $entityManager,
+        Request $requete
+    ): Response
+    {
+        $wish = new Wish();
+        $wishForm = $this->createForm(WishType::class, $wish);
+        $wishForm->handleRequest($requete);
+
+        $wish->setIsPublished(true);
+        $wish->setDate(new \DateTime());
+        $wish->setAuteur("Caliendo Julien");
+
+        if ($wishForm->isSubmitted() && $wishForm->isValid()) {
+            $entityManager->persist($wish);
+            $entityManager->flush();
+            return $this->redirectToRoute('wish_details', ['wish' => $wish->getId()]);
+        }
+        return $this->render(
+            'wish/nouveau.html.twig',
+            compact('wishForm')
+        );
+    }
+
+
+
+
+
 }
---
+++ src/Entity/Wish.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WishRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WishRepository::class)]
+class Wish
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 250)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $auteur = null;
+
+    #[ORM\Column]
+    private ?bool $isPublished = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    public function __construct()
+    {
+        $this->date = new \DateTime();
+        $this->isPublished = true;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): static
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function isIsPublished(): ?bool
+    {
+        return $this->isPublished;
+    }
+
+    public function setIsPublished(bool $isPublished): static
+    {
+        $this->isPublished = $isPublished;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+
+}
